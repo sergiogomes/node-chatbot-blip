0cfa24008be65fd9918137c6eb4b4a2ce8622462
sergiogomes <sergiopgomes92@gmail.com>
2020-12-15 16:20:47 -0300

    Some changings.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import * as BlipSdk from 'blip-sdk';
-import * as WebSocketTransport from 'lime-transport-websocket'
+const BlipSdk = require('blip-sdk')
+const WebSocketTransport = require('lime-transport-websocket')
 
 const IDENTIFIER = 'bb83';
 const ACCESS_KEY = 'b3ZrMk5mUFQwSWZZOGdtQkRUUHc=';
@@ -11,9 +11,9 @@ const client = new BlipSdk.ClientBuilder()
   .build();
 
 // Connection will occurr via websocket on 8081 port asynchronously.
-client.connect() // This method return a 'promise'.
-  .then(function (session) {
+client.connect()
+  .then((session) => {
     // Connection success. Now is possible send and receive envelopes from server. */
     console.log('Application started. Press Ctrl + c to stop.')
   })
-  .catch(function (err) { /* Connection failed. */ });
+  .catch((err) => { /* Connection failed. */ });
